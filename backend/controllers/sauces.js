f3c01dd8ecb9763f4e5b44de5bae641bcf2f8e6a
Julien Frebet <frebet.julien@gmail.com>
2022-05-21 12:51:13 +0200

    like dislike ok

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -85,74 +85,24 @@ exports.getAllSauces = (req, res, next) => {
     });
 };
 
-// exports.sauceLiked = (req, res, next) => {
-//   const userId = req.body.userId;
-//   const like = req.body.like;
-//   // console.log(userId);
-
-//   Sauce.findOne({
-//     _id: req.params.id,
-//   })
-//     .then((sauce) => {
-//       // if (like == 0) {
-//       //   //  newUsersLiked = usersLiked.filter((userId) => {
-//       //   //    if (usersLiked._id !== userId){
-
-//       //   //      return  usersLiked;
-//       //   //       console.log(usersLiked);
-//       //   //    }
-//       //   //   })
-//       //   newUsersDisliked = usersDisiked.filter((user) => {
-//       //     if (user._id !== userId) {
-//       //       return user;
-//       //       console.log(user);
-//       //     }
-//       //     usersDisiked.push(user);
-//       //   });
-
-//       //   // usersDisliked.filter(req.body.userId);
-//       // } else
-//       if (like == 1) {
-//         sauce.like++;
-//         console.log(usersDisiked);
-//         res.status(200).json();
-//         // sauce.userliked.push(req.body.userId);
-//       } else if (like == -1) {
-//         sauce.dislike--;
-//         // sauce.userliked.push(req.body.userId);
-//       }
-//     })
-//     .catch((error) => {
-//       res.status(404).json({
-//         error: error,
-//       });
-//     });
-
-//   res.status(200).json({});
-// };
-
 exports.sauceLiked = (req, res, next) => {
   console.log(req.body.like);
 
   const sauceId = req.params.id;
   const userId = req.body.userId;
 
-  sauce
-    .findOne({ _id: sauceId })
+  Sauce.findOne({ _id: sauceId })
     .then((el) => {
-      res.status(200).json(el);
-
       if (!el.usersLiked.includes(userId) && req.body.like == 1) {
         // si userId n'est pas dans userslike et qu on envoie 1
         console.log("je suis a 1");
-        sauce
-          .updateOne(
-            { _id: sauceId },
-            {
-              $inc: { likes: 1 },
-              $push: { usersLiked: userId },
-            }
-          )
+        Sauce.updateOne(
+          { _id: sauceId },
+          {
+            $inc: { likes: 1 },
+            $push: { usersLiked: userId },
+          }
+        )
 
           .then(() => res.status(201).json({ message: "user like 1" }))
 
@@ -162,42 +112,39 @@ exports.sauceLiked = (req, res, next) => {
         // si userId est pas dans usersdislike et qu on envoie 0
         console.log("je suis a 0");
 
-        sauce
-          .updateOne(
-            { _id: sauceId },
-            {
-              $inc: { likes: 0 },
-              $pull: { usersLiked: userId },
-            }
-          )
+        Sauce.updateOne(
+          { _id: sauceId },
+          {
+            $inc: { likes: -1 },
+            $pull: { usersLiked: userId },
+          }
+        )
           .then(() => res.status(201).json({ message: "user like 0" }))
           .catch((error) => res.status(400).json({ error }));
       }
       if (!el.usersDisliked.includes(userId) && req.body.like == -1) {
         // si userId n'est pas dans usersdislike et qu on envoie -1
         console.log("je suis a -1");
-        sauce
-          .updateOne(
-            { _id: sauceId },
-            {
-              $inc: { dislikes: 1 },
-              $push: { usersDisliked: userId },
-            }
-          )
+        Sauce.updateOne(
+          { _id: sauceId },
+          {
+            $inc: { dislikes: 1 },
+            $push: { usersDisliked: userId },
+          }
+        )
           .then(() => res.status(201).json({ message: "user like -1" }))
           .catch((error) => res.status(400).json({ error }));
       }
       if (el.usersDisliked.includes(userId) && req.body.like == 0) {
         // si userId est pas dans usersdislike et qu on envoie 0
         console.log("je suis a 0 ici");
-        sauce
-          .updateOne(
-            { _id: sauceId },
-            {
-              $inc: { dislikes: 0 },
-              $pull: { usersDisliked: userId },
-            }
-          )
+        Sauce.updateOne(
+          { _id: sauceId },
+          {
+            $inc: { dislikes: -1 },
+            $pull: { usersDisliked: userId },
+          }
+        )
           .then(() => res.status(201).json({ message: "user like 0 ici" }))
           .catch((error) => res.status(400).json({ error }));
       }
